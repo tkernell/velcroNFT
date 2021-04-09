b9c4399b9d9646042dc631e90253b039aed48276
Tim <tpkernell@gmail.com>
2021-04-09 16:59:40 -0500

    Initialize some contracts, fill in some of PlanController

--- contracts/PlanController.sol
+++ contracts/PlanController.sol
@@ -2,9 +2,13 @@
 
 pragma solidity 0.8.0;
 
+// import ""
 import "./SubscriptionNFT.sol";
+import "./UserPool.sol";
+import "./ProviderPool.sol";
 
 contract PlanController {
+    address planFactory;
     SubscriptionNFT subNFT;
     address userPool;
     address providerPool;
@@ -18,7 +22,15 @@ contract PlanController {
     
     mapping(address => subToken) subTokens;
     
-    constructor(SubscriptionNFT _subNFT, address _userPool, address _providerPool, uint256 _period) {
+    constructor(uint256 _period, address _planFactory) {
+        planFactory = _planFactory;
+        period = _period;
+        subNFT = new SubscriptionNFT(address(this));
+        userPool = address(new UserPool());
+        providerPool = address(new ProviderPool(address(this)));
+    }
+    
+    function initialize(SubscriptionNFT _subNFT, address _userPool, address _providerPool, uint256 _period) public {
         subNFT = _subNFT;
         userPool = _userPool;
         providerPool = _providerPool;
--- contracts/SubscriptionNFT.sol
+++ contracts/SubscriptionNFT.sol
@@ -5,5 +5,20 @@ pragma solidity 0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract SubscriptionNFT is ERC721 {
-    constructor() ERC721("Subscription", "SUB") {}
+    address owner;
+    uint256 counter;
+    
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+    
+    constructor(address _owner) ERC721("Subscription", "SUB") {
+        owner = _owner;
+    }
+    
+    function mint(address _to) public onlyOwner {
+        _mint(_to, counter);
+        counter++;
+    }
 }
