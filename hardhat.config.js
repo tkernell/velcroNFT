ede09333282606d2b4508893f53d54d06b06c4bd
Tim <tpkernell@gmail.com>
2021-04-28 20:02:05 -0500

    hardhat network forking

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,5 +4,12 @@ require("@nomiclabs/hardhat-waffle");
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.0"
+  solidity: "0.8.0",
+  networks: {
+    hardhat: {
+      forking: {
+        url: "https://eth-kovan.alchemyapi.io/v2/{key}"
+      }
+    }
+  }  
 };
